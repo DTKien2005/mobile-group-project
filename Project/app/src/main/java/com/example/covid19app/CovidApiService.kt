6cd139dded2f1571ab60937f59171d49a19dfd64
thentran5781 <anhtt.22bi13042@usth.edu.vn>
2025-09-28 19:17:08 +0700

    Update 30-Day Trends and Symptom Checker

---
+++ Project/app/src/main/java/com/example/covid19app/CovidApiService.kt
@@ -0,0 +1,19 @@
+package com.example.covid19app
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+data class HistoricalResponse(
+    val timeline: Timeline
+)
+
+data class Timeline(
+    val cases: Map<String, Int>,
+    val deaths: Map<String, Int>,
+    val recovered: Map<String, Int>
+)
+
+interface CovidApiService {
+    @GET("v3/covid-19/historical/Vietnam?lastdays=30")
+    fun getVietnamTrends(): Call<HistoricalResponse>
+}
--- Project/app/src/main/java/com/example/covid19app/MainActivity.kt
+++ Project/app/src/main/java/com/example/covid19app/MainActivity.kt
@@ -2,76 +2,73 @@ package com.example.covid19app
 
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.covid19app.ui.theme.Covid19AppTheme
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-private const val TAG = "MainActivity"
+class MainActivity : AppCompatActivity() {
 
-class MainActivity : ComponentActivity() {
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(TAG, "onCreate(Bundle?) called")
-        enableEdgeToEdge()
-        setContent {
-            Covid19AppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+        Log.d(TAG, "onCreate called")
+        setContentView(R.layout.activity_main)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        if (savedInstanceState == null) {
+            loadFragment(TrendsFragment())
+        }
+
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_trends -> {
+                    loadFragment(TrendsFragment())
+                    true
                 }
+                R.id.nav_symptom -> {
+                    loadFragment(SymptomCheckerFragment())
+                    true
+                }
+                else -> false
             }
         }
     }
 
     override fun onStart() {
         super.onStart()
-        Log.d(TAG, "onStart() called")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "onPause() called")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "onStop() called")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy() called")
+        Log.d(TAG, "onStart called")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d(TAG, "onResume() called")
+        Log.d(TAG, "onResume called")
     }
 
-}
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause called")
+    }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop called")
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Covid19AppTheme {
-        Greeting("Android")
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart called")
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
