46cd218f7d8978ec418ad1598196eb1abcbe8698
DTKien2005 <kiendt2005.ict@gmail.com>
2025-10-05 00:56:00 +0700

    Back to previous (I forget to delete :(((( )

--- Project/app/src/main/java/com/example/covid19app/CovidApi.kt
+++
@@ -1,9 +0,0 @@
-package com.example.covid19app
-
-import retrofit2.Call
-import retrofit2.http.GET
-
-interface CovidApi {
-    @GET("v3/covid-19/countries")
-    fun getCountries(): Call<List<Country>>
-}
